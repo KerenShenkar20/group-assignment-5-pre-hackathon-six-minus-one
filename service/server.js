9b674e9f3692061f8053006dc075bfd0882cb264
Sagi <chuboksagi@gmail.com>
2020-12-17 08:18:26 +0200

    fix structure

--- service/server.js
+++ service/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const logger = require("morgan");
+const { userRouter } = require("./routers/user.router");
 //const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { userRouter } = require("./routers/user.router");
 
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
-const router = express.Router();
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Content-Type','application/json');
+    next();
+ });
+
 app.use('/api/users', userRouter);
-//app.use((req,res,next)=>{
-//    res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//    res.set('Content-Type','application/json');
-//    next();
-//});
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
